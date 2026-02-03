bc0e97b45d9fc2404145827bc5138407d4b0b341
Mansi Singh <mansisingh.singh14@gmail.com>
2026-02-03 12:12:25 +0530

    Time: 18 ms (5.87%), Space: 12.6 MB (56.1%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def countAndSay(self, n):
+        s = '1'
+        for _ in range(n - 1):
+            s = re.sub(r'(.)\1*', lambda m: str(len(m.group(0))) + m.group(1), s)
+        return s
+        """
+        :type n: int
+        :rtype: str
+        """
+        
