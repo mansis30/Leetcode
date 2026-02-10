9471f9094d5e52ec96c6fa85302c0c07b3da0ff8
Mansi Singh <mansisingh.singh14@gmail.com>
2026-02-11 00:13:33 +0530

    Time: 0 ms (100%), Space: 12.3 MB (89.16%) - LeetHub

---
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findLUSlength(self, a, b):
+        if a == b:
+            return -1
+        return max(len(a), len(b))
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        
