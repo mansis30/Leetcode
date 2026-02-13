67803435c5503720d77a223c410275610595aa62
Mansi Singh <mansisingh.singh14@gmail.com>
2026-02-13 11:41:23 +0530

    Time: 7 ms (36.51%), Space: 12.5 MB (55.87%) - LeetHub

---
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -0,0 +1,10 @@
+class Solution(object):
+        def shortestToChar(self, S, C):
+            n, pos = len(S), -float('inf')
+            res = [n] * n
+            for i in range(n) + range(n)[::-1]:
+                if S[i] == C:
+                    pos = i
+                res[i] = min(res[i], abs(i - pos))
+            return res
+        
