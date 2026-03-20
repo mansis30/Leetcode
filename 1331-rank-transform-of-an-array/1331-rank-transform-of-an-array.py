1ba12020085cd5d6fa225ffa2a457d9a67596a01
Mansi Singh <mansisingh.singh14@gmail.com>
2026-03-20 11:54:54 +0530

    Time: 63 ms (25.38%), Space: 28 MB (73.6%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def arrayRankTransform(self, A):
+        rank = {}
+        for a in sorted(A):
+            rank.setdefault(a, len(rank) + 1)
+        return map(rank.get, A)
+        
