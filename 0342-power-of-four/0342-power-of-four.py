8518bb5fac68398a801177af0cf9e6a6b9064e4e
Mansi Singh <mansisingh.singh14@gmail.com>
2026-05-07 00:11:04 +0530

    Time: 50 ms (2.37%), Space: 19.5 MB (7.74%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n <= 0:
+            return False
+            
+        return math.log(n, 4).is_integer()
+        
+        
