4db3fc6f20b7456adb3ac0661c197dfdb833c0d7
Mansi Singh <mansisingh.singh14@gmail.com>
2026-05-26 01:09:33 +0530

    Time: 35 ms (27.43%), Space: 27.7 MB (13.83%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -10,15 +10,15 @@ class Solution:
         l[0] = 1
         r[-1] = 1
 
-        # Prefix products
+        
         for i in range(1, n):
             l[i] = nums[i - 1] * l[i - 1]
 
-        # Suffix products
+        
         for i in range(n - 2, -1, -1):
             r[i] = nums[i + 1] * r[i + 1]
 
-        # Final result
+       
         op = [0] * n
 
         for i in range(n):
