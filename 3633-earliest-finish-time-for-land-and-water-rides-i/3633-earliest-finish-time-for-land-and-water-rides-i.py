1bca83e6d4d9ac49bc56e8e5a1a46b050e565718
Mansi Singh <mansisingh.singh14@gmail.com>
2026-06-03 00:10:00 +0530

    Time: 507 ms (45.95%), Space: 12.4 MB (37.84%) - LeetHub

---
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def earliestFinishTime(self, landStartTime, landDuration,
+                           waterStartTime, waterDuration):
+
+        ans = float('inf')
+        for i in range(len(landStartTime)):
+            for j in range(len(waterStartTime)):
+
+                land_finish = landStartTime[i] + landDuration[i]
+                finish1 = max(land_finish, waterStartTime[j]) + waterDuration[j]
+
+                water_finish = waterStartTime[j] + waterDuration[j]
+                finish2 = max(water_finish, landStartTime[i]) + landDuration[i]
+
+                ans = min(ans, finish1, finish2)
+
+        return ans
