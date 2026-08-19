1add8a76fdc13014ef96e06f67b903fff027706d
Mansi Singh <mansisingh.singh14@gmail.com>
2026-08-19 23:30:53 +0530

    Time: 173 ms (5.97%), Space: 15 MB (100%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -0,0 +1,44 @@
+class Solution:
+    def maxNumberOfFamilies(self, n, reservedSeats):
+
+        reservedSeats.sort()
+
+        m = len(reservedSeats)
+
+        reserved = [False] * 11
+
+        ans = 0
+        l = 0
+        r = 0
+        prev = 0
+
+        def check(left, right):
+            for seat in range(left, right + 1):
+                if reserved[seat]:
+                    return False
+            return True
+
+        while r < m:
+            ans += 2 * (reservedSeats[r][0] - prev - 1)
+
+            prev = reservedSeats[r][0]
+
+            while r < m and reservedSeats[r][0] == reservedSeats[l][0]:
+                reserved[reservedSeats[r][1]] = True
+                r += 1
+
+            two_to_five = check(2, 5)
+            four_to_seven = check(4, 7)
+            six_to_nine = check(6, 9)
+
+            if two_to_five and six_to_nine:
+                ans += 2
+            elif two_to_five or four_to_seven or six_to_nine:
+                ans += 1
+
+            l = r
+            reserved = [False] * 11
+        ans += 2 * (n - prev)
+
+        return ans
+        
