dfc2e0ee4d61f58d7a1cc7e2ee92eb1ab25b84f4
Mansi Singh <mansisingh.singh14@gmail.com>
2026-08-25 00:53:52 +0530

    Time: 808 ms (81.82%), Space: 24.3 MB (50%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.py
+++ 1872-stone-game-viii/1872-stone-game-viii.py
@@ -4,18 +4,13 @@ class Solution:
 
         prefix = stones[:]
 
-        # Build prefix sums
         for i in range(1, n):
             prefix[i] += prefix[i - 1]
 
-        # If Alice takes all stones,
-        # the game ends immediately.
         best = prefix[n - 1]
 
-        # Try every earlier valid prefix
+
         for i in range(n - 2, 0, -1):
             best = max(best, prefix[i] - best)
 
         return best
-
-        
